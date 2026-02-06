38597590ce5460364a04d997b13ebce9696679e6
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-06 20:31:15 +0530

    assignments

---
+++ 03-assignment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR=$1
+
+if [ -z "$DIR" ]; then
+  echo "Usage: $0 <directory>"
+  exit 1
+fi
+
+if [ ! -d "$DIR" ]; then
+  echo "Error: Directory does not exist"
+  exit `1
+fi
+
+find "$DIR" -type f -name "*.conf" -exec sed -i 's/FOO/BAR/g' {} +
