db736f1eb3daaf9194aa4f6a704532f78a4ff035
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-08 19:26:54 +0530

    ansible

--- 01-assignment.sh
+++ 01-assignment.sh
@@ -2,36 +2,24 @@
 
 FILE=$1
 
-# Check if argument is given
-if [ $# -eq 0 ]; then
-    echo "Please provide file or directory name"
-    exit 1
-fi
-
-# Check existence
-if [ -e "$FILE" ]; then
-    echo "File exists: $FILE"
-else
-    echo "File not present: $FILE"
-    exit 1
-fi
-
-# Check readable
-if [ -r "$FILE" ]; then
-    echo "Readable: YES"
-else
-    echo "Readable: NO"
-fi
-
-# Check writable
-if [ -w "$FILE" ]; then
-    echo "Writable: YES"
-else
-    echo "Writable: NO"
-fi
-
-# If directory, list files
 if [ -d "$FILE" ]; then
-    echo "It is a directory. Files inside:"
-    ls -l "$FILE"
+    echo "Given path: $FILE is a directory"
+    echo "Files inside the directory:"
+    ls "$FILE"
+
+elif [ -f "$FILE" ]; then
+    echo "Given path: $FILE is a file"
+
+    if [ -r "$FILE" ] && [ -w "$FILE" ]; then
+        echo "The file is readable and writable"
+    elif [ -r "$FILE" ]; then
+        echo "The file is readable but not writable"
+    elif [ -w "$FILE" ]; then
+        echo "The file is writable but not readable"
+    else
+        echo "The file is neither readable nor writable"
+    fi
+
+else
+    echo "Given path: $FILE does not exist"
 fi
